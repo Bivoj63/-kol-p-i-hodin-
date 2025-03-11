2f034cd3b57efe8a0324cd40d0ff963f4165605f
unknown <martin.friedel@infis.cz>
2025-03-11 08:31:55 +0100

    poznámky

---
+++ poznámky.py
@@ -0,0 +1,17 @@
+#přetypování na int(číslo)
+něco = int(input("Zadejte číslo: "))
+#první podmínka if a použítí or(nebo)
+if něco < 5 or něco ==5:
+    print("menší jak nebo rovno 5")
+#druhá podmínka elif
+elif něco > 6:
+    print("Větší jak 6")
+#třetí podmínka else
+else:
+    print("rovno šest")
+
+#Martin Friedel
+
+#git add *
+#git commit -m "poznámky"
+#git push
